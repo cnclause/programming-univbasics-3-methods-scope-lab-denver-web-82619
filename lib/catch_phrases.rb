efa761816fee977b8a233efc6b83cd0c3d34766f
Catherine Clause <cnclause123@gmail.com>
2019-08-19 13:32:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,4 +6,9 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end 
+
+def link 
+  help = "It's Dangerous To Go Alone! Take This."
+  puts help
 end
